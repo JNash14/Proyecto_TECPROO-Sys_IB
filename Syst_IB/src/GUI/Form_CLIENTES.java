27984f215960cf65c3eca3c11ee98412354de447
JNash14 <carcelixjon@gmail.com>
2025-09-14 15:32:34 -0500

    Actualización: creación de carpeta clases y clase Clientes

--- Syst_IB/src/GUI/Form_CLIENTES.java
+++ Syst_IB/src/GUI/Form_CLIENTES.java
@@ -44,22 +44,22 @@ public class Form_CLIENTES extends JFrame {
 	public Form_CLIENTES() {
 		setTitle("CLIENTES");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 577, 531);
+		setBounds(100, 100, 577, 497);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Apellidos:");
-		lblNewLabel.setBounds(25, 25, 64, 14);
+		lblNewLabel.setBounds(25, 43, 64, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNombres = new JLabel("Nombres:");
-		lblNombres.setBounds(25, 49, 64, 14);
+		lblNombres.setBounds(25, 71, 64, 14);
 		contentPane.add(lblNombres);
 		
 		JLabel lblDni = new JLabel("DNI:");
-		lblDni.setBounds(25, 74, 64, 14);
+		lblDni.setBounds(25, 14, 64, 14);
 		contentPane.add(lblDni);
 		
 		JLabel lblCelular = new JLabel("Celular:");
@@ -71,18 +71,18 @@ public class Form_CLIENTES extends JFrame {
 		contentPane.add(lblDireccin);
 		
 		textField = new JTextField();
-		textField.setBounds(99, 22, 200, 20);
+		textField.setBounds(99, 40, 200, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(99, 46, 200, 20);
+		textField_1.setBounds(99, 68, 200, 20);
 		contentPane.add(textField_1);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(99, 71, 122, 20);
+		textField_2.setBounds(99, 11, 122, 20);
 		contentPane.add(textField_2);
 		
 		textField_3 = new JTextField();
@@ -96,7 +96,7 @@ public class Form_CLIENTES extends JFrame {
 		contentPane.add(textField_4);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(25, 270, 503, 186);
+		scrollPane.setBounds(25, 243, 503, 186);
 		contentPane.add(scrollPane);
 		
 		JTextArea textArea = new JTextArea();
@@ -106,24 +106,24 @@ public class Form_CLIENTES extends JFrame {
 		btnRegistrar.setBounds(395, 49, 103, 49);
 		contentPane.add(btnRegistrar);
 		
-		JButton btnListar = new JButton("Listar");
-		btnListar.setBounds(25, 216, 89, 31);
-		contentPane.add(btnListar);
+		JButton btnReportar = new JButton("Reportar");
+		btnReportar.setBounds(25, 183, 89, 31);
+		contentPane.add(btnReportar);
 		
 		JButton btnAdicionar = new JButton("Adicionar");
-		btnAdicionar.setBounds(124, 216, 89, 31);
+		btnAdicionar.setBounds(124, 183, 89, 31);
 		contentPane.add(btnAdicionar);
 		
 		JButton btnBuscar = new JButton("Buscar");
-		btnBuscar.setBounds(223, 216, 89, 31);
+		btnBuscar.setBounds(223, 183, 89, 31);
 		contentPane.add(btnBuscar);
 		
 		JButton btnModificar = new JButton("Modificar");
-		btnModificar.setBounds(330, 216, 89, 31);
+		btnModificar.setBounds(330, 183, 89, 31);
 		contentPane.add(btnModificar);
 		
 		JButton btnEliminar = new JButton("Eliminar");
-		btnEliminar.setBounds(439, 216, 89, 31);
+		btnEliminar.setBounds(439, 183, 89, 31);
 		contentPane.add(btnEliminar);
 
 	}
